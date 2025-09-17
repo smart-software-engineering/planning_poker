1e0af5a24a7779844c0340ac44920deaf1cddb4c
Rico Metzger <rico.metzger@gmail.com>
2025-09-17 20:46:55 +0200

    Update layouts.ex

--- lib/planning_poker_web/components/layouts.ex
+++ lib/planning_poker_web/components/layouts.ex
@@ -47,7 +47,7 @@ defmodule PlanningPokerWeb.Layouts do
     </header>
 
     <main class="px-4 py-12 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-4xl space-y-4">
+      <div class="mx-auto max-w-full lg:max-w-7xl space-y-4">
         {render_slot(@inner_block)}
       </div>
     </main>
